edd15266c69efa1f6a5ba3ccb2ff1b0543acb75a
Al-Taie <agprosup@gmail.com>
2022-08-04 15:38:30 +0300

    NPI-1644 implement HmsMessageService on HuaweiService.kt

--- gms/src/main/java/com/gateway/gms/data/HuaweiService.kt
+++ gms/src/main/java/com/gateway/gms/data/HuaweiService.kt
@@ -1,10 +1,15 @@
 package com.gateway.gms.data
 
+import com.gateway.gms.di.GMServiceLocator
 import com.gateway.gms.domain.interfaces.CloudMessaging
 import com.gateway.gms.domain.models.MessagingTask
+import com.huawei.hms.push.HmsMessageService
 import com.huawei.hms.push.HmsMessaging
+import com.huawei.hms.push.RemoteMessage
 
-class HuaweiService(private val messaging: HmsMessaging) : CloudMessaging {
+open class HuaweiService : CloudMessaging, HmsMessageService() {
+    private val messaging: HmsMessaging by lazy { GMServiceLocator.huaweiService }
+
     override fun subscribeToTopic(topic: String) =
         safeTaskCall { MessagingTask.Huawei(messaging.subscribe(topic)) }
 
@@ -14,4 +19,12 @@ class HuaweiService(private val messaging: HmsMessaging) : CloudMessaging {
     override fun getToken() = safeTaskCall { MessagingTask.Huawei<Nothing>(null) }
 
     override fun deleteToken() = safeTaskCall { MessagingTask.Huawei<Nothing>(null) }
+
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+    }
+
+    override fun onNewToken(p0: String?) {
+        super.onNewToken(p0)
+    }
 }
