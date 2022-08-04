dd738026372244593bc5c85ad5f559a1c3e111cb
Al-Taie <agprosup@gmail.com>
2022-08-04 15:38:02 +0300

    NPI-1644 implement FirebaseMessagingService on GoogleService.kt

--- gms/src/main/java/com/gateway/gms/data/GoogleService.kt
+++ gms/src/main/java/com/gateway/gms/data/GoogleService.kt
@@ -1,10 +1,16 @@
 package com.gateway.gms.data
 
+import com.gateway.gms.di.GMServiceLocator
 import com.gateway.gms.domain.interfaces.CloudMessaging
 import com.gateway.gms.domain.models.MessagingTask
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import timber.log.Timber
 
-class GoogleService(private val messaging: FirebaseMessaging) : CloudMessaging {
+open class GoogleService : CloudMessaging, FirebaseMessagingService() {
+    private val messaging: FirebaseMessaging by lazy { GMServiceLocator.googleService }
+
     override fun subscribeToTopic(topic: String) =
         safeTaskCall { MessagingTask.Google(messaging.subscribeToTopic(topic)) }
 
@@ -14,4 +20,15 @@ class GoogleService(private val messaging: FirebaseMessaging) : CloudMessaging {
     override fun getToken() = safeTaskCall { MessagingTask.Google(messaging.token) }
 
     override fun deleteToken() = safeTaskCall { MessagingTask.Google(messaging.deleteToken()) }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Timber.d(message.toString())
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Timber.d(token)
+
+    }
 }
